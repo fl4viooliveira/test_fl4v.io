50805785c4eef46fe68af05fed65fa9f0c904ead
fl4viooliveira <fl4viooliveira@gmail.com>
2021-08-11 23:33:55 +0100

    Post List View

---
+++ frontend/src/components/Loader.js
@@ -0,0 +1,12 @@
+import { Dimmer, Loader, Image, Segment } from "semantic-ui-react";
+
+export default () => {
+  return (
+    <Segment>
+      <Dimmer active inverted>
+        <Loader inverted>Loading</Loader>
+      </Dimmer>
+      <Image src="https://react.semantic-ui.com/images/wireframe/short-paragraph.png" />
+    </Segment>
+  );
+};
